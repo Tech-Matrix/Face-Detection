928e831c32920d465c6a20f97a1fc6e71e29a8e4
urmilkalaria <urmilkalaria@gmail.com>
2021-09-18 09:40:47 +0530

    Added Emotion Recognition and Eyes detection Feature

--- LiveVideoFaceDetection.py
+++ LiveVideoFaceDetection.py
@@ -1,24 +1,79 @@
-
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
 import cv2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# command line argument
+ap = argparse.ArgumentParser()
+ap.add_argument("--mode",help="display")
+mode = ap.parse_args().mode
 
-cap=cv2.VideoCapture(0)
-cap.set(3,1280)
-cap.set(4,1050)
-cap.set(10,100)
-while True:
-    success, img = cap.read()
+# Create the model
+model = Sequential()
 
-    gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-    faces=face_cascade.detectMultiScale(gray, 1.8, 5)
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-    for (x,y,w,h) in faces:
-         cv2.rectangle(img, (x, y), (x+w, y+h), (252, 1, 2), 3)
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
 
-    cv2.imshow("video",img)
-    if cv2.waitKey(1) & 0xFF ==ord('q'):
-     break
-cap.release()
+# emotions will be displayed on your face from the webcam feed
+if mode == "display":
+    model.load_weights('model.h5')
 
+    # prevents openCL usage and unnecessary logging messages
+    cv2.ocl.setUseOpenCL(False)
+
+    # dictionary which assigns each label an emotion (alphabetical order)
+    emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+    # start the webcam feed
+    face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    eye_cascade=cv2.CascadeClassifier('haarcascade_eye.xml')
+
+
+    cap=cv2.VideoCapture(0)
+    cap.set(3,1280)
+    cap.set(4,1050)
+    cap.set(10,100)
+    while True:
+        success, img = cap.read()
+
+        gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        faces=face_cascade.detectMultiScale(gray, 1.3, 4)
+        eye = eye_cascade.detectMultiScale(gray, 1.3,4)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (252, 1, 2), 3)
+            roi_gray = gray[y:y+h,x:x+w]
+            roi_color = img[y:y+h,x: x+w]
+            eyes = eye_cascade.detectMultiScale(roi_gray, 1.03,2)
+            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+            prediction = model.predict(cropped_img)
+            maxindex = int(np.argmax(prediction))
+            cv2.putText(img, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0,255,0),1)
+        cv2.imshow("video",img)
+        if cv2.waitKey(1) & 0xFF ==ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
